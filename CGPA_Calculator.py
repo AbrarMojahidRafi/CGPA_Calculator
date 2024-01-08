f2f7358025dd03aa782451130c372c45b657e4de
AbrarMojahidRafi <rafi.cse.bracu@gmail.com>
2024-01-09 00:37:46 +0600

    all the files are uploading

---
+++ CGPA_Calculator.py
@@ -0,0 +1,159 @@
+import PySimpleGUI as sg
+import functions
+
+sg.theme("DarkGreen5")
+
+enterTheCourseName = sg.Text("Enter the Course Code: ", font=("Arial Bold", 10))
+courseNameInputBox = sg.Input("", key="courseNameInputBox", do_not_clear=False)
+
+EnterTheCGPAOfThatCourse = sg.Text("Enter the CGPA of that Course: ", font=("Arial Bold", 10))
+cgpa_list = ["0.0", "0.7", "1.0", "1.3", "1.7", "2.0", "2.3", "2.7", "3.0", "3.3", "3.7", "4.0"]
+CGPAOfThatCourse = sg.Combo(cgpa_list,
+                            key="CGPAOfThatCourse",
+                            size=(10, 1))
+creditOfThatCourse = sg.Text("Credit: ", font=("Arial Bold", 10))
+credit_list = ["1", "2", "3", "4", "5"]
+creditOfThatCourseCombo = sg.Combo(credit_list, default_value="3", key="creditOfThatCourseCombo", size=(10, 1))
+
+retakeText = sg.Text("Did you retake this course?", font=("Arial Bold", 10))
+retakeCombo = sg.Combo(['Yes', 'No'], default_value="No", key="retakeCombo", size=(5, 1))
+
+addButton = sg.Button("Add", font=("Arial Bold", 10))
+
+with open("courses.txt", "r") as courses:
+    listBox = sg.Listbox(courses.readlines(), size=(55, 10), font=("Arial Bold", 10), key="list_Box",
+                         enable_events=True)
+
+editButton = sg.Button("Edit", font=("Arial Bold", 10))
+removeButton = sg.Button("Remove", font=("Arial Bold", 10))
+
+calculate = sg.Button("Calculate the CGPA", font=("Arial Bold", 10))
+
+window = sg.Window("CGPA Calculator",
+                   layout=[
+                       [enterTheCourseName, courseNameInputBox],
+                       [EnterTheCGPAOfThatCourse, CGPAOfThatCourse, creditOfThatCourse, creditOfThatCourseCombo],
+                       [retakeText, retakeCombo, addButton],
+                       [listBox, editButton, removeButton],
+                       [calculate]
+                   ]
+                   )
+
+while True:
+    event = window.Read()
+    cases, inputValues = event
+    print(event)
+    if cases == sg.WINDOW_CLOSED:
+        break
+    if cases == "list_Box":
+        try:
+            if len(inputValues["list_Box"][0].split(" || ")) == 2:
+                courseName, cgpa = inputValues["list_Box"][0].split(" || ")
+                window["courseNameInputBox"].update(courseName)
+                window["CGPAOfThatCourse"].update(cgpa.strip("\n"))
+                window["retakeCombo"].update("No")
+            elif len(inputValues["list_Box"][0].split(" || ")) == 3:
+                courseName, cgpa, credit = inputValues["list_Box"][0].split(" || ")
+                window["courseNameInputBox"].update(courseName)
+                window["CGPAOfThatCourse"].update(cgpa)
+                window["creditOfThatCourseCombo"].update(credit.strip("\n"))
+                window["retakeCombo"].update("No")
+            elif len(inputValues["list_Box"][0].split(" || ")) == 4:
+                courseName, cgpa, credit, retake = inputValues["list_Box"][0].split(" || ")
+                window["courseNameInputBox"].update(courseName)
+                window["CGPAOfThatCourse"].update(cgpa)
+                window["creditOfThatCourseCombo"].update(credit)
+                window["retakeCombo"].update("Yes")
+        except:
+            pass
+    elif cases == "Add":
+        if inputValues["retakeCombo"] == "No":
+            if functions.check_course_code_present_or_not(inputValues["courseNameInputBox"]):
+                sg.popup_error('Please give the COURSE CODE.')
+            elif functions.check_cgpa_is_valid_or_not(inputValues["CGPAOfThatCourse"]):
+                if len(inputValues["""CGPAOfThatCourse"""]) != 0:
+                    sg.popup_error(
+                        f'Please give the CGPA properly of your course.\nYou write "{inputValues["""CGPAOfThatCourse"""]}"'
+                    )
+                else:
+                    sg.popup_error(
+                        f'Please give the CGPA properly of your course.\nYou write "noting", which is not correct.'
+                    )
+            elif functions.check_credit_is_valid_or_not(inputValues["creditOfThatCourseCombo"]):
+                sg.popup_error('Your credit is not valid.\nValid Credit is 1-10.')
+            elif functions.check_duplicate(inputValues["courseNameInputBox"]):
+                sg.popup_error('This course already in the list.')
+            else:
+                functions.adding_new_course(inputValues["courseNameInputBox"],
+                                            inputValues["CGPAOfThatCourse"],
+                                            inputValues["creditOfThatCourseCombo"]
+                                            )
+                window["list_Box"].update(functions.getting_courses())
+                window['CGPAOfThatCourse'].update(value='')
+                window["creditOfThatCourseCombo"].update(value="3")
+                window["retakeCombo"].update("No")
+        else:
+            functions.adding_retake_course(inputValues["courseNameInputBox"],
+                                           inputValues["CGPAOfThatCourse"],
+                                           inputValues["creditOfThatCourseCombo"]
+                                           )
+            window["list_Box"].update(functions.getting_courses())
+            window['CGPAOfThatCourse'].update(value='')
+            window["creditOfThatCourseCombo"].update(value="3")
+            window["retakeCombo"].update("No")
+    elif cases == "Calculate the CGPA":
+        grade = functions.calculating_cgpa()
+        sg.popup_scrolled(grade,
+                          title="Your grades")
+    elif cases == "Edit":
+        if inputValues["retakeCombo"] == "No":
+            if functions.check_course_code_present_or_not(inputValues["courseNameInputBox"]):
+                sg.popup_error('Please give the COURSE CODE.')
+            elif functions.check_cgpa_is_valid_or_not(inputValues["CGPAOfThatCourse"]):
+                if len(inputValues["""CGPAOfThatCourse"""]) != 0:
+                    sg.popup_error(
+                        f'Please give the CGPA properly of your course.\nYou write "{inputValues["""CGPAOfThatCourse"""]}"'
+                    )
+                else:
+                    sg.popup_error(
+                        f'Please give the CGPA properly of your course.\nYou write "noting", which is not correct.'
+                    )
+            elif functions.check_credit_is_valid_or_not(inputValues["creditOfThatCourseCombo"]):
+                sg.popup_error('Your credit is not valid.\nValid Credit is 1-10.')
+            elif functions.check_duplicate(inputValues["courseNameInputBox"]):
+                sg.popup_error('This course already in the list.')
+            else:
+                functions.editing_a_course_or_its_cgpa(
+                    inputValues["list_Box"][0],
+                    inputValues["courseNameInputBox"],
+                    inputValues["CGPAOfThatCourse"],
+                    inputValues["creditOfThatCourseCombo"]
+                )
+                window["list_Box"].update(functions.getting_courses())
+                window['CGPAOfThatCourse'].update(value='')
+                window["creditOfThatCourseCombo"].update(value="3")
+                window["retakeCombo"].update("No")
+        else:
+            print(inputValues)
+            functions.editing_for_retake(inputValues["list_Box"][0],
+                                         inputValues["courseNameInputBox"],
+                                         inputValues["CGPAOfThatCourse"],
+                                         inputValues["creditOfThatCourseCombo"]
+                                         )
+            window["list_Box"].update(functions.getting_courses())
+            window['CGPAOfThatCourse'].update(value='')
+            window["creditOfThatCourseCombo"].update(value="3")
+            window["retakeCombo"].update("No")
+
+    elif cases == "Remove":
+        try:
+            if inputValues["list_Box"][0] != "Course || CGPA\n":
+                functions.removing_selected_course(inputValues["list_Box"][0])
+                window["list_Box"].update(functions.getting_courses())
+            else:
+                # give a popup window
+                pass
+            window['CGPAOfThatCourse'].update(value='')
+            window["creditOfThatCourseCombo"].update(value="3")
+        except:
+            sg.popup_error('Select one course.')
---
+++ functions.py
@@ -0,0 +1,106 @@
+def getting_courses():
+    """will return a courses list"""
+    with open("courses.txt", "r") as file_opening:
+        return file_opening.readlines()
+
+
+def writing_new_course(courses):
+    with open("courses.txt", "w") as file:
+        for i in courses:
+            file.write(i)
+
+
+# buttons functionality
+def adding_new_course(course_name_input_box, cgpa_of_that_course, credit_of_that_course_combo):
+    courses = getting_courses()
+    courses.append(f"{course_name_input_box} || {cgpa_of_that_course} || {credit_of_that_course_combo}\n")
+    writing_new_course(courses)
+
+
+def adding_retake_course(course_name_input_box, cgpa_of_that_course, credit_of_that_course_combo):
+    courses = getting_courses()
+    for i in range(len(courses)):
+        if courses[i].split(" || ")[0] == course_name_input_box:
+            courses[i] = (f"{course_name_input_box} || {cgpa_of_that_course} || {credit_of_that_course_combo}"
+                          f" || Before Retake, CGPA was {courses[i].split(' || ')[1]}\n"
+                          )
+    writing_new_course(courses)
+
+
+def calculating_cgpa():
+    courses = getting_courses()
+    denominator_number_of_credit = 0
+    numerator = 0
+    grade = "COURSE || CGPA\n"
+    for i in range(1, len(courses)):
+        course, cgpa, credit = courses[i].split(" || ")
+        denominator_number_of_credit += int(credit)
+        numerator += (float(cgpa)*int(credit))
+        grade = grade + f"{course} || {cgpa}\n"
+    grade = grade + "--------------------------------------------------\n"
+    cg = numerator / denominator_number_of_credit
+    grade = grade + f"Your current CGPA:- {cg}\n"
+    grade = grade + f"Total Completed Credit:- {denominator_number_of_credit}"
+    return grade
+
+
+def editing_a_course_or_its_cgpa(course_prev, course_name_update, cgpa_update, credit_of_that_course_combo):
+    courses = getting_courses()
+    for i in range(len(courses)):
+        if courses[i].split(" || ")[0] == course_prev.split(" || ")[0]:
+            courses[i] = f"{course_name_update} || {cgpa_update} || {credit_of_that_course_combo}\n"
+            break
+    writing_new_course(courses)
+
+
+def editing_for_retake(course_prev, course_name_update, cgpa_update, credit_of_that_course_combo):
+    courses = getting_courses()
+    for i in range(len(courses)):
+        if courses[i].split(" || ")[0] == course_prev.split(" || ")[0]:
+            courses[i] = (f"{course_name_update} || {cgpa_update} || {credit_of_that_course_combo} "
+                          f"|| Before Retake, CGPA was {course_prev.split(' || ')[1]}\n")
+    writing_new_course(courses)
+
+
+def removing_selected_course(course):
+    courses = getting_courses()
+    for i in range(len(courses)):
+        if courses[i] == course:
+            courses.remove(course)
+            break
+    writing_new_course(courses)
+
+
+# Error handling of Add button
+def check_course_code_present_or_not(course):
+    if len(course) == 0:
+        return True  # means user didn't give the course code.
+
+
+def check_cgpa_is_valid_or_not(cgpa):
+    if len(cgpa) == 0:
+        return True
+    elif "0.0" > cgpa or "4.0" < cgpa:
+        return True
+    else:
+        flag = False
+        for i in cgpa:
+            if i not in ".0123456789":
+                flag = True
+
+        if flag == True:
+            return True
+        else:
+            return False
+
+
+def check_credit_is_valid_or_not(credit):
+    if int(credit) < 1 or int(credit) > 10:
+        return True
+
+
+def check_duplicate(course):
+    courses = getting_courses()
+    for i in courses:
+        if i.split(" || ")[0] == course:
+            return True
